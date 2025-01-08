3306a7df45e77d324f5437f29640f94854911f41
sheltertake <sheltertake@gmail.com>
2025-01-08 16:32:57 +0100

    feat: reducer switch

--- 06-lists/lists/src/App.jsx
+++ 06-lists/lists/src/App.jsx
@@ -28,14 +28,15 @@ const getAsyncStories = () =>
   );
 
 const storiesReducer = (state, action) => {
-  if (action.type === 'SET_STORIES') {
-    return action.payload;
-  } else if (action.type === 'REMOVE_STORY') {
-    return state.filter(
-      (story) => action.payload.objectID !== story.objectID
-    );
-  } else {
-    throw new Error();
+  switch (action.type) {
+    case 'SET_STORIES':
+      return action.payload;
+    case 'REMOVE_STORY':
+      return state.filter(
+        (story) => action.payload.objectID !== story.objectID
+      );
+    default:
+      throw new Error();
   }
 };
 
