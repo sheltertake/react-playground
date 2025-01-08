caf8e20efaddd334ff6eeaa7b31e98bc14546a0e
sheltertake <sheltertake@gmail.com>
2025-01-08 14:37:31 +0100

    feat: destructuring via spread operator

--- 06-lists/lists/src/App.jsx
+++ 06-lists/lists/src/App.jsx
@@ -49,11 +49,7 @@ const List = ({list}) => (
   <ul>
     {list?.map((item) => (
       <Item key={item.objectID} 
-        title={item.title}
-        url={item.url}
-        author={item.author}
-        num_comments={item.num_comments}
-        points={item.points} 
+        {...item}
       />
     ))}
   </ul>
