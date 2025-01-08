c4c2d8563f8bc6673ddeb870c74febb311d8b476
sheltertake <sheltertake@gmail.com>
2025-01-08 12:20:12 +0100

    feat: handler

--- 06-lists/lists/src/App.jsx
+++ 06-lists/lists/src/App.jsx
@@ -26,12 +26,20 @@ const App = () => (
   </div>
 );
 
-const Search = () => (
-  <>
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text" />
-  </>
-);
+const Search = () => {
+  const handleChange = (event) => {
+    // synthetic event
+    console.log(event);
+    // value of target (here: input HTML element)
+    console.log(event.target.value);
+  };
+  return (
+    <>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+    </>
+  );
+};
 const List = () => (
   <ul>
     {list.map(function (item) {
