bd3cf0a75c9737805df9897ae93cf6eb812ee33f
sheltertake <sheltertake@gmail.com>
2025-01-08 11:03:50 +0100

    feat: helloworld + list app

---
+++ 05-hw/helloworld/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </div>
+  );
+}
+export default App;
