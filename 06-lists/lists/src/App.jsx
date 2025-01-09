069b1e7069adf42b0863d631329d3d9fe33924dc
sheltertake <sheltertake@gmail.com>
2025-01-09 09:55:57 +0100

    feat: fetch data from api

--- 06-lists/lists/src/App.jsx
+++ 06-lists/lists/src/App.jsx
@@ -1,31 +1,10 @@
 import React from 'react';
 
-const initialStories = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
-const getAsyncStories = () =>
-  new Promise((resolve) =>
-    setTimeout(
-      () => resolve({ data: { stories: initialStories } }),
-      2000
-    )
-  );
+const getAsyncStories = (searchTerm) =>
+  fetch(`${API_ENDPOINT}${searchTerm}`)
+  .then((response) => response.json());
 
 const storiesReducer = (state, action) => {
   switch (action.type) {
@@ -84,17 +63,19 @@ const App = () => {
   );
 
   React.useEffect(() => {
+    if (!searchTerm) return;
+
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    getAsyncStories()
+    getAsyncStories(searchTerm)
       .then(result => {
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.data.stories,
+          payload: result.hits,
         });
       })
       .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
-  }, []);
+  }, [searchTerm]);
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
--- 08-ts/lists/src/App.tsx
+++ 08-ts/lists/src/App.tsx
@@ -39,32 +39,11 @@ type StoriesFetchSuccessAction = {
 
 type StoriesAction = StoriesFetchStatesAction | StoriesFetchSuccessAction | StoriesRemoveAction;
 
-const initialStories: Stories = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
-const getAsyncStories = (): Promise<{ data: { stories: Stories } }> =>
-  new Promise((resolve) =>
-    setTimeout(
-      () => resolve({ data: { stories: initialStories } }),
-      2000
-    )
-  );
+const getAsyncStories = (searchTerm: string): Promise<{ hits: Stories }> =>
+  fetch(`${API_ENDPOINT}${searchTerm}`)
+    .then((response) => response.json());
 
 const storiesReducer = (state: StoriesState, action: StoriesAction) => {
   switch (action.type) {
@@ -115,7 +94,6 @@ const useStorageState = (
 
 const App = () => {
 
-
   const [searchTerm, setSearchTerm] = useStorageState('search', 'React');
 
   const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -128,17 +106,19 @@ const App = () => {
   );
 
   React.useEffect(() => {
+    if (!searchTerm) return;
+
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    getAsyncStories()
+    getAsyncStories(searchTerm)
       .then(result => {
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.data.stories,
+          payload: result.hits,
         });
       })
       .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
-  }, []);
+  }, [searchTerm]);
 
 
   const searchedStories = stories.data.filter((story: Story) =>
